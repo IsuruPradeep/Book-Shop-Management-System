248607001c20f78a434fc67969772ed115a04838
IsuruPradeep <pisuru136@gmail.com>
2025-05-11 08:58:19 +0530

    login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,27 +1,38 @@
 import React from 'react'
 
+
 const Login = () => {
   return (
-    <div class="bg-white text-gray-500 max-w-96 mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10">
-    <h2 class="text-2xl font-semibold mb-6 text-center text-gray-800">Welcome back</h2>
-    <form>
-        <input id="email" class="w-full bg-transparent border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4" type="email" placeholder="Enter your email" required/>
-        <input id="password" class="w-full bg-transparent border mt-1 border-gray-500/30 outline-none rounded-full py-2.5 px-4" type="password" placeholder="Enter your password" required/>
-        <div class="text-right py-4">
-            <a class="text-blue-600 underline" href="#">Forgot Password</a>
+    <div className='h-[calc(100vh-120px)] flex justyfy-center items-center'>
+
+        <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+            <h2 className='text-xl font-semibold mb-4'>Please Login</h2>
+            <form>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2
+                    htmlFor="email"'>Email</label>
+                    <input type="email" name="email" id='email' placeholder='Email Address'
+                    className='sahdow appearence-none border rounded w-full py-2 px-3
+                    leading-tight focuse:outline-none focus:shadow'
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2
+                    htmlFor="password"'>Password</label>
+                    <input type="password" name="password" id='passsword' placeholder='Password'
+                    className='sahdow appearence-none border rounded w-full py-2 px-3
+                    leading-tight focuse:outline-none focus:shadow'
+                    />
+                </div>
+                <div>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2
+                    px-8 rounded focus:outline-none'>Login</button>
+                </div>
+            </form>
+            <p className='align-baseline font-medium mt-4 text-sm'>Haven't account? please Register</p>
         </div>
-        <button type="submit" class="w-full mb-3 bg-indigo-500 py-2.5 rounded-full text-white">Log in</button>
-    </form>
-    <p class="text-center mt-4">Don’t have an account? <a href="#" class="text-blue-500 underline">Signup</a></p>
-    <button type="button" class="w-full flex items-center gap-2 justify-center mt-5 bg-black py-2.5 rounded-full text-white">
-        <img class="h-4 w-4" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/login/appleLogo.png" alt="appleLogo"/>
-        Log in with Apple
-    </button>
-    <button type="button" class="w-full flex items-center gap-2 justify-center my-3 bg-white border border-gray-500/30 py-2.5 rounded-full text-gray-800">
-        <img class="h-4 w-4" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/login/googleFavicon.png" alt="googleFavicon"/>
-        Log in with Apple
-    </button>
-</div>
+
+    </div>
   )
 }
 
