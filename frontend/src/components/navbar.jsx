6e6a6efb7dcaaf09f39bcfa74feb0ee9aeba7103
IsuruPradeep <pisuru136@gmail.com>
2025-05-16 05:22:14 +0530

    logout part

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,6 +7,7 @@ import { PiHeartFill } from "react-icons/pi";
 import { PiShoppingCartDuotone } from "react-icons/pi";
 import avatarImg from "../assets/avatar.png";
 import { useSelector } from 'react-redux';
+import { useAuth } from '../context/AuthContext';
 
 
 
@@ -20,7 +21,13 @@ const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
   const cartItems= useSelector(state => state.cart.cartItems);
 
-  const currentUser=false;
+  const {currentUser, logout} = useAuth()
+
+  const handleLogOut = () => {
+    logout();
+    setIsDropdownOpen(false);
+  }
+
   return (
     <header className="max-w-screen-2xl mx-auto px-10 py-6">
       <nav className="flex justify-between items-center">
@@ -74,6 +81,12 @@ const Navbar = () => {
                     </li>
                   ))
                 }
+                <li>
+                  <button 
+                  onClick={handleLogOut}
+                  className="block w-full text-left px-4 py-2 text-sm
+                   hover:bg-gray-100">Logout</button>
+                </li>
               </ul>
             </div>
           )
